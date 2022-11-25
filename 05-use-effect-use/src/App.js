061382a6ce187ee73c5d53352e6acbd4bbe56068
Udit8158 <kunduudit8158@gmail.com>
2022-11-25 18:45:45 +0530

    some change

--- 05-use-effect-use/src/App.js
+++ 05-use-effect-use/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // Using useeffect for taking data as a side effect
+  useEffect(() => {
+    const loginInformation = localStorage.getItem("login-information");
+    setIsLoggedIn(loginInformation === "1" ? true : false);
+  }, [isLoggedIn]);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("login-information", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("login-information", "0");
     setIsLoggedIn(false);
   };
 
