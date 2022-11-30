24b6e163daf9da88c8cc6e96c6cdca7c2d54cda9
Udit8158 <kunduudit8158@gmail.com>
2022-11-30 23:02:15 +0530

    working on food app again, and now stroe meals data in firebase db

--- 06-food-order-app/src/components/Meals/AvailableMeals.js
+++ 06-food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,13 +1,55 @@
-import DUMMY_MEALS from "./dummy-meals";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const DB_URL =
+    "https://react-http-request-26266-default-rtdb.firebaseio.com/meals.jsons";
+
+  const fetchMeals = async () => {
+    setLoading(true);
+
+    const response = await fetch(DB_URL);
+
+    if (!response.ok) {
+      throw new Error("Some thing went wrong");
+    }
+
+    const data = await response.json();
+
+    let transformedData = [];
+
+    for (const key in data) {
+      transformedData.push({
+        id: key,
+        name: data[key].name,
+        description: data[key].description,
+        price: data[key].price,
+      });
+    }
+
+    setMeals(transformedData);
+  };
+
+  useEffect(() => {
+    fetchMeals().catch((err) => {
+      setError({ msg: err.message });
+      setLoading(false);
+    });
+  }, []);
   return (
     <Card className={classes.meals}>
+      {!error && loading && (
+        <p className={classes["loading-text"]}>Loading...</p>
+      )}
+      {error && <p className={classes["error-text"]}>{error.msg}</p>}
       <ul>
-        {DUMMY_MEALS.map((meal) => {
+        {meals.map((meal) => {
           return (
             <MealItem
               id={meal.id}
